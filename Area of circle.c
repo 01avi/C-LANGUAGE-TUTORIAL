38eccad21750900e74be2e09926e99d5bcb6ac6e
Aviral <110219413+01avi@users.noreply.github.com>
2022-08-03 10:23:15 +0530

    if else

--- Area of circle.c
+++ Area of circle.c
@@ -3,13 +3,13 @@ main()
 {
 	 float p=3.14;
 	 float R,A;
+	     printf("PLEASE ENTER THE VALUE OF THE RADIOUS OF CIRCLE:- ");
+	     scanf("%f", &R);
+         if(R>=0){
+	         A= p*R*R;
+             printf("AREA OF THE CIRCLE IS:- %f", A);
+        }else{
+	        printf("Enter valid radius!!!");
+    }
+}
   
-	 printf("PLEASE ENTER THE VALUE OF THE RADIOUS OF CIRCLE:- ");
-	 scanf("%f", &R);
-  
-	 A= p*R*R;
-  
-	 printf("AREA OF THE CIRCLE IS:- %f", A);
-  
- }
-	
