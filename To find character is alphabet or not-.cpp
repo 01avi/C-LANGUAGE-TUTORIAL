881766b6d36610e004411626a8df5447e0259578
Aviral <110219413+01avi@users.noreply.github.com>
2022-08-09 11:14:40 +0530

    if esle

--- To find character is alphabet or not-.cpp
+++ To find character is alphabet or not-.cpp
@@ -1,17 +1,13 @@
 #include<stdio.h>
 main()
 {
-	char ch;
-	printf("enter any character");
-	scanf("%c", &ch);;
-	
-	if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
-	{
-		printf("character is alphabet");
-	}
-	else
-	{
-		printf("character is not an alphabet");
-	}
-	
+  char ch;
+  printf("enter any character:- ");	
+  scanf("%c", &ch);
+     if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')){
+	 printf("character is alphabet");
+     }
+     else {
+	 printf("character is not an alphabet");
+     }
 }
