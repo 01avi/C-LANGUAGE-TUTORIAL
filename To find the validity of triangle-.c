e64ef7cd069821e65751eb8251030efa8468997a
Aviral <110219413+01avi@users.noreply.github.com>
2022-08-14 10:14:46 +0530

    nested if

--- To find the validity of triangle-.c
+++ To find the validity of triangle-.c
@@ -2,15 +2,25 @@
 main()
 {
 	int Angle1, Angle2, Angle3, SUM;
-	printf("Enter Angles of triangle=\n");
-	scanf("%d%d%d", &Angle1, &Angle2, &Angle3);
+	printf("Enter Angle 1 of triangle:-\n ");
+	scanf("%d", &Angle1);
+	printf("Enter Angle 2 of triangle:-\n ");
+	scanf("%d", &Angle2);
+	printf("Enter Angle 3 of triangle:-\n ");
+	scanf("%d", &Angle3);
+	
 	SUM=Angle1+ Angle2+ Angle3;
-	if(SUM==180 &&Angle1>0 &&Angle2>0 &&Angle3>0)
-	{
-		printf("triangle is valid");
-	}
-	else
-	{
-		printf("triangle is not valid");
+	
+	if(SUM==180 && Angle1>0 && Angle2>0 && Angle3>0){
+		
+		printf("This triangle is valid");
+		
+	}else if(Angle1==60 || Angle1==90 && Angle2==60 || Angle2==90 && Angle3==60|| Angle3==90){
+		
+		printf("This triangle is valid and it is a right angle triangle");
+		
+	}else{
+		
+		printf("This triangle is not valid");
 	}
 }
