d4057441b46766f3d302a972c3f57c60e25935f5
Aviral <110219413+01avi@users.noreply.github.com>
2022-09-06 10:05:32 +0530

    Do While loop

---
+++ Looping Statement/Alphabet checking-.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+main()
+{
+	char ch;
+
+	printf("ENTER ANY CHARACTER:-");
+		do{
+	scanf("%c",&ch);
+
+	
+	if(ch=='a')
+	{
+		printf("VOWEL");
+	}
+	
+	
+    else if(ch=='A')
+    {
+		printf("VOWEL");
+	}
+	
+	
+
+	   else if(ch=='e')
+	{
+		printf("VOWEL");
+	}
+	
+	
+	
+	   else if(ch=='E')
+	{
+		printf("VOWEL");
+	}
+	
+	
+	
+	   else if(ch=='i')
+	{
+		printf("VOWEL");
+	}
+	
+	
+	   else if(ch=='I')
+    {
+		printf("VOWEL");
+	}
+	
+	
+	   else if(ch=='O')
+	{
+		printf("VOWEL");
+	}
+	
+	
+	   else if(ch=='O')
+	{
+		printf("VOWEL");
+	}
+	
+	
+	   else if(ch=='u')
+	{
+		printf("VOWEL");
+	}
+	
+	
+	   else if(ch=='U')
+	   {
+		printf("VOWEL");
+	}
+	
+	
+	    else if(ch=='0')
+	printf("THANK YOU FOR USING");
+	
+
+	else
+	{
+		printf("CONSONANET");
+	}
+
+}while(ch=='0');
+}
