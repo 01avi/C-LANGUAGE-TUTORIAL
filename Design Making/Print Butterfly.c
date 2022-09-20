504a6acb0c0eb64aec02b91355332ed0390d45cc
Aviral <110219413+01avi@users.noreply.github.com>
2022-09-20 10:33:30 +0530

    Add files via upload

---
+++ Design Making/Print Butterfly.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+main()
+{
+	int n,i,j;
+	printf("Enter Pyramid length:- ");
+	scanf("%d", &n);
+	
+	       //upper half;
+
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i;j++){
+              printf("*");
+        }
+
+        int space=2*(n-i);
+        for(j=1;j<=space;j++){
+            printf(" ");
+        }
+
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+
+           printf("\n");
+        }
+               
+             //lower half;
+
+        for(i=n;i>=1;i--){
+            for(j=1;j<=i;j++){
+              printf("*");
+           }
+
+        int space=2*(n-i);
+        for(j=1;j<=space;j++){
+           printf(" ");
+        }
+
+        for(j=1;j<=i;j++){
+           printf("*");
+        }
+
+           printf("\n");
+   }
+}
+       
