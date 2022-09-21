2ea37ff75d2f1d2effff1770f296169b5598d389
Aviral <110219413+01avi@users.noreply.github.com>
2022-09-21 10:01:30 +0530

    If else

--- Decision Making/Alphabet checking-.cpp
+++ Decision Making/Alphabet checking-.cpp
@@ -2,7 +2,7 @@
 main()
 {
 	char ch;
-	printf("enter any character:- ");
+	printf("Enter any character:- ");
 	scanf("%c", &ch);
 	if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u')||(ch=='A')||(ch=='E')||(ch=='I')||(ch=='O')||(ch=='U'))
 	{
