d45b2643cd32bce0a7f2a07b7fe2d7adf6d369f3
Aviral <110219413+01avi@users.noreply.github.com>
2022-09-24 09:34:58 +0530

    For loop

--- Looping Statement/Print all even no till-.c
+++ Looping Statement/Print all even no till-.c
@@ -2,9 +2,9 @@
 main()
 {
 	int i,n;
-	printf("PRINT ALL EVEN NUMBERS TILL:--");
+	printf("PRINT ALL EVEN NUMBERS TILL:- ");
 	scanf("%d", &n);
-	printf("EVEN NUMBERS FROM 1 TO %d:--\n", n);
+	printf("EVEN NUMBERS FROM 1 TO %d:- \n", n);
 	for(i=1; i<=n; i++)
 	{
 		if(i%2!=1)
