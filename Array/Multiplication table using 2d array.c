eed93b4b8bc67ddbb3f4d3988162561852d88cb4
Aviral <110219413+01avi@users.noreply.github.com>
2022-10-12 23:58:47 +0530

    2d array

--- Array/Multiplication table using 2d array.c
+++ Array/Multiplication table using 2d array.c
@@ -15,8 +15,8 @@ int main()
 	printf("\n");
 	printf("MULTIPLICATION TABLE IS:-");
 	for(j=s;j<=b;j++){
-		printf("\n");
-		for(i=0;i<a;i++){
+	       printf("\n");
+               for(i=0;i<a;i++){
 			row=i+1;
 			printf("%2d|",row);
 			for(j=s;j<=b;j++){
