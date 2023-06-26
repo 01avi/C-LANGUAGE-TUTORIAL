a47255af26c13b1266e9978424179770d7112c5d
Aviral <110219413+01avi@users.noreply.github.com>
2023-06-26 21:51:28 +0530

    Update Alphabet checking-.cpp

--- Decision Making/Alphabet checking-.cpp
+++ Decision Making/Alphabet checking-.cpp
@@ -3,7 +3,7 @@ main()
 {
 	char ch;
 	printf("Enter any character:- ");
-	scanf("%c", &ch);u
+	scanf("%c", &ch);
 	if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u')||(ch=='A')||(ch=='E')||(ch=='I')||(ch=='O')||(ch=='U'))
 	{
 		printf("character is vowel");
