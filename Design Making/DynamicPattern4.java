300fc850fa99843ef742b945c9852ee43c8e273d
Aviral <110219413+01avi@users.noreply.github.com>
2025-04-28 06:51:21 +0530

    Update DynamicPattern4.java

--- Design Making/DynamicPattern4.java
+++ Design Making/DynamicPattern4.java
@@ -25,7 +25,7 @@ public class DynamicPattern4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number:- ");
-        int n = 5;
+        int n = sc.nextInt();
         int i, j;
 
         for (i = 0; i < n / 2 + 1; i++) {
